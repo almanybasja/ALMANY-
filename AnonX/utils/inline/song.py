5555861c52431133c8044773bba09248456694d0
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-06-24 01:02:26 +0200

    Update song.py

--- AnonX/utils/inline/song.py
+++ AnonX/utils/inline/song.py
@@ -15,7 +15,7 @@ def song_markup(_, vidid):
         ],
         [
             InlineKeyboardButton(
-                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url=f"{config.SUPPORT_GROUP}",
+                text="[𖢿التحديثات𖢿]", url=f"https://t.me/HL_BG",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
