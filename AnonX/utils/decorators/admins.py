ca46d0e90d4f716d1f43d66f5599e45e46d9624a
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-06-25 04:56:02 +0200

    Update admins.py

--- AnonX/utils/decorators/admins.py
+++ AnonX/utils/decorators/admins.py
@@ -19,7 +19,7 @@ def AdminRightsCheck(mystic):
         if await is_maintenance() is False:
             if message.from_user.id not in SUDOERS:
                 return await message.reply_text(
-                    "» ʙᴏᴛ ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ ғᴏʀ sᴏᴍᴇ ᴛɪᴍᴇ, ᴩʟᴇᴀsᴇ ᴠɪsɪᴛ sᴜᴩᴩᴏʀᴛ ᴄʜᴀᴛ ᴛᴏ ᴋɴᴏᴡ ᴛʜᴇ ʀᴇᴀsᴏɴ."
+                    "» البوت تحت الصيانه بعض الوقت يرجى الانتظار او زريادة فريق الدعم لمهعرفت السبب."
                 )
         if await is_commanddelete_on(message.chat.id):
             try:
@@ -80,7 +80,7 @@ def AdminActual(mystic):
         if await is_maintenance() is False:
             if message.from_user.id not in SUDOERS:
                 return await message.reply_text(
-                    "» ʙᴏᴛ ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ ғᴏʀ sᴏᴍᴇ ᴛɪᴍᴇ, ᴩʟᴇᴀsᴇ ᴠɪsɪᴛ sᴜᴩᴩᴏʀᴛ ᴄʜᴀᴛ ᴛᴏ ᴋɴᴏᴡ ᴛʜᴇ ʀᴇᴀsᴏɴ."
+                    "» البوت تحت الصيانه بعض الوقت يرجى الانتظار او زريادة فريق الدعم لمهعرفت السبب."
                 )
         if await is_commanddelete_on(message.chat.id):
             try:
@@ -125,7 +125,7 @@ def ActualAdminCB(mystic):
         if await is_maintenance() is False:
             if CallbackQuery.from_user.id not in SUDOERS:
                 return await CallbackQuery.answer(
-                    "» ʙᴏᴛ ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ ғᴏʀ sᴏᴍᴇ ᴛɪᴍᴇ, ᴩʟᴇᴀsᴇ ᴠɪsɪᴛ sᴜᴩᴩᴏʀᴛ ᴄʜᴀᴛ ᴛᴏ ᴋɴᴏᴡ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
+                     "» البوت تحت الصيانه بعض الوقت يرجى الانتظار او زريادة فريق الدعم لمهعرفت السبب."
                     show_alert=True,
                 )
         try:
