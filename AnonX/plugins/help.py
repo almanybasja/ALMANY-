7277c049ddfe9c236257ab24dcdf9499086ae2a5
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-06-26 08:48:40 +0200

    Update help.py

--- AnonX/plugins/help.py
+++ AnonX/plugins/help.py
@@ -58,7 +58,7 @@ async def helper_private(
         language = await get_lang(chat_id)
         _ = get_string(language)
         keyboard = help_pannel(_)
-        await update.reply_text(f"مرحبا بك عزيزي {Message.from_user.mention} في قائمة المساعده")
+        await update.reply_text(f"مرحبا بك عزيزي {message.from_user.mention} في قائمة المساعده")
         await update.reply_photo(
             photo=config.START_IMG_URL,
             caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard)
