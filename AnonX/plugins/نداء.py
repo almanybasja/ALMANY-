b336b25ea1d03884c4415e7a1ca173f6026546f6
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-06-26 02:07:10 +0200

    Update نداء.py

--- AnonX/plugins/نداء.py
+++ AnonX/plugins/نداء.py
@@ -26,6 +26,6 @@ def call_random_member(client:Client, message:Message):
         f"• يـا راس السطل تعال {random_member_mention}",
         f"• انت ليش قمر هكي 🌚♥ {random_member_mention}"
     ])
-    client.send_message(chat_id, random_message, reply_to_message_id=message.message_id, parse_mode='markdown')
+    # client.send_message(chat_id, random_message, reply_to_message_id=message.message_id, parse_mode='markdown')
 
 
