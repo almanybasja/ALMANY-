efbeff3fac1523f9692a6b27ac91bfdc96b57bbf
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-07-06 11:37:19 +0200

    Update logger.py

--- AnonX/utils/logger.py
+++ AnonX/utils/logger.py
@@ -3,24 +3,24 @@ from AnonX import app
 from AnonX.utils.database import is_on_off
 from pyrogram.types import Message
 
-async def play_logs(Message, streamtype):
+async def play_logs(message, streamtype):
     if await is_on_off(LOG):
-        if Message.chat.username:
-            chatusername = f"@{Message.chat.username}"
+        if message.chat.username:
+            chatusername = f"@{message.chat.username}"
         else:
             chatusername = "محادثه خاصه"
         logger_text = f"""
 **{MUSIC_BOT_NAME} مسجل التشغيل**
 
-**الدردشه:** {Message.chat.title} [`{Message.chat.id}`]
-**المعرف:** @{Message.from_user.username}
-**الايدي:** `{Message.from_user.id}`
+**الدردشه:** {message.chat.title} [`{message.chat.id}`]
+**المعرف:** @{message.from_user.username}
+**الايدي:** `{message.from_user.id}`
 **رابط الدردشه:** {chatusername}
 
-**تم البحث بواسطة:** {Message.text}
+**تم البحث بواسطة:** {message.text}
 
 **نوع المشغل:** {streamtype}"""
-        if Message.chat.id != LOG_GROUP_ID:
+        if message.chat.id != LOG_GROUP_ID:
             try:
                 await app.send_message(
                     LOG_GROUP_ID,
