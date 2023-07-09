82ea7ae47d5e02cf29ad022552e110b18945e483
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-07-09 15:21:43 +0200

    Update twettc.py

--- AnonX/plugins/twettc.py
+++ AnonX/plugins/twettc.py
@@ -3689,3 +3689,33 @@ async def cutt(client: Client, message: Message):
 
         f"{a}")
         
+iddof = []
+@app.on_message(
+    command(["قفل كت","تعطيل تويت"])
+    & filters.group
+    & ~filters.edited
+)
+async def iddlock_two(client, message):
+   get = await app.get_chat_member(message.chat.id, message.from_user.id)
+   if get.status in ["creator", "administrator"]:
+      if message.chat.id in iddof:
+        return await message.reply_text("تم معطل من قبل🔒")
+      iddof.append(message.chat.id)
+      return await message.reply_text("تم تعطيل كت بنجاح ✅🔒")
+   else:
+      return await message.reply_text("لازم تكون ادمن يشخه علشان اسمع كلامك")
+@app.on_message(
+    command(["قفل كت","تعطيل تويت"])
+    & filters.group
+    & ~filters.edited
+)
+async def iddlock_two(client, message):
+   get = await app.get_chat_member(message.chat.id, message.from_user.id)
+   if get.status in ["creator", "administrator"]:
+      if message.chat.id in iddof:
+        return await message.reply_text("تم معطل من قبل🔒")
+      iddof.append(message.chat.id)
+      return await message.reply_text("تم تعطيل كت بنجاح ✅🔒")
+   else:
+      return await message.reply_text("لازم تكون ادمن يشخه علشان اسمع كلامك")
+    
