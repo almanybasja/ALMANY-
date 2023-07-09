612211602007e946b2f95cac0acd22105565ac23
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-07-09 18:15:16 +0200

    Update id.py

--- AnonX/plugins/id.py
+++ AnonX/plugins/id.py
@@ -38,7 +38,7 @@ async def iddlock(client:Client, message):
     get = await client.get_chat_member(message.chat.id, message.from_user.id)
     if get.status in ["creator", "administrator"]:
         if message.chat.id in iddof:
-        return await message.reply_text("الايدي مفعل من قبل ✅")
+           return await message.reply_text("الايدي مفعل من قبل ✅")
       iddof.remove(message.chat.id)
       return await message.reply_text("تم فتح الايدي بنجاح ✅🔓")
  
