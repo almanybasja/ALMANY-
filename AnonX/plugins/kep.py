f72fffd564093f5da3dc24ed7b7b1006cfcb8ffd
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-07-09 17:50:42 +0200

    Update kep.py

--- AnonX/plugins/kep.py
+++ AnonX/plugins/kep.py
@@ -2,17 +2,17 @@ from strings.filters import command
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from AnonX import app, Telegram
-from config import *
+import config
 
 # تعيين القناة الخاصة وزر الاشتراك
 
-subscribe_button = InlineKeyboardButton("اشترك في القناة", url="https://t.me/{CHANNEL_DEV}")
+subscribe_button = InlineKeyboardButton("اشترك في القناة", url="https://t.me/{config.CHANNEL_DEV}")
 
 # دالة لمعالجة الأمر /start
 @Client.on_message(filters.command("start"))
 async def start(client, message):
     # الرد على الامر /start بالأزرار المطلوبة
-    if message.from_user.id == int(OWNER_ID):
+    if message.from_user.id == (config.OWNER_ID):
         await message.reply("مرحباً بك! اليك الأزرار المطلوبة",
                             reply_markup=InlineKeyboardMarkup([[subscribe_button]]))
     else:
