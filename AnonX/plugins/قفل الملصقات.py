99a439fdfb8e6046bbee5501150220e464b4ba20
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-07-10 18:20:00 +0200

    Create قفل الملصقات.py

---
+++ AnonX/plugins/قفل الملصقات.py
@@ -0,0 +1,19 @@
+from strings.filters import command
+from AnonX import app
+from pyrogram import Client, Filters
+
+
+
+    # يجب أن يرسل الأمر المشترك إلى المجموعة لتنفيذه
+    @app.on_message(Filters.group)
+    def block_stickers(client, message):
+        # يتم حجب الملصقات بالحذف في المجموعة
+        if message.text == "قفل الملصقات":
+            client.set_chat_permissions(message.chat.id, can_send_stickers=False)
+
+        # يتم فتح الملصقات بعدم الحذف في المجموعة
+        elif message.text == "فتح الملصقات":
+            client.set_chat_permissions(message.chat.id, can_send_stickers=True)
+
+        # إرسال رسالة استجابة للإعلان عن تمكين أمر قفل وفتح الملصقات
+        client.send_message(message.chat.id, "تم تنفيذ الأمر!")
