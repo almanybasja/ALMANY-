ee44452fd8ad99fae2ef97cfac3dc4295a685bde
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-07-10 18:13:48 +0200

    Update and rename حساب.py to احسب عمري.py

--- AnonX/plugins/احسب عمري.py
+++ AnonX/plugins/احسب عمري.py
@@ -1,3 +1,9 @@
+###########
+#####كود كاتبة  الوسكي الليبي#####
+##########
+
+
+
 import asyncio
 import pyrogram
 import random
@@ -17,12 +23,12 @@ from pyrogram.types import (InlineKeyboardButton,
 
 
 
-@app.on_message(filters.regex(r'احسب عمري'))
+@app.on_message(filters.regex(r'احسب عمري'))###كتابة الوسكي من الصفر
 async def calculate_age(client:Client, message:Message):
     try:
-        birth_date = datetime.datetime.strptime(message.text.split(" ")[2], "%d-%m-%Y")
+        birth_date = datetime.datetime.strptime(message.text.split(" ")[2], "%d-%m-%Y")##### ماتروح تتعب بدل ماتخمط السورسات ياخول
     except:
-        await message.reply_text(f"يا {message.from_user.mention} انت كاتب الامر خطأ اكتبه هكذا (احسب عمري 1-1-2000)")
+        await message.reply_text(f"**يا {message.from_user.mention} انت كاتب الامر خطأ اكتبه هكذا (احسب عمري 1-1-2000)**")
         return
 
     alyawm = datetime.datetime.today()
@@ -34,4 +40,4 @@ async def calculate_age(client:Client, message:Message):
         next_birthday = datetime.datetime(alyawm.year+1, birth_date.month, birth_date.day)
     remaining_days = (next_birthday - alyawm).days
 
-    await message.reply_text(f"مبروك يا {message.from_user.mention} • عمرك هو >> {age} سنه\n\n  •عمرك ب الاشهر >> {months} شهر \n\n  •عمرك ب الايام >> {days} يوم.\n\n  •عيد ميلادك بعد >> {remaining_days} يوم. \n\n ماتعزمنا ياولد")
+    await message.reply_text(f"مبروك يا {message.from_user.mention} • عمرك هو → {age} سنه\n\n  •عمرك ب الاشهر → {months} شهر \n\n  •عمرك ب الايام → {days} يوم.\n\n  •عيد ميلادك بعد → {remaining_days} يوم. \n\n ماتعزمنا ياولد")
