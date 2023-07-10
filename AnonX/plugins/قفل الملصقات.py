43554e3413d995fb28f06659ad72e07e77c2142f
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-07-11 01:15:28 +0200

    Update قفل الملصقات.py

--- AnonX/plugins/قفل الملصقات.py
+++ AnonX/plugins/قفل الملصقات.py
@@ -2,8 +2,8 @@ from strings.filters import command
 from AnonX import app
 from pyrogram import Client, filters
 from pyrogram.types import Message
-
-stiklok =["creator","administrator"]
+from pyrogram.enums import ParseMode, ChatMemberStatus
+stiklok =[]
 
 @app.on_message(
     filters.command(["قفل الملصقات","تعطيل الملصقات"])
@@ -12,7 +12,7 @@ stiklok =["creator","administrator"]
 )
 async def block_stickers(client:Client, message:Message):
     get = await client.get_chat_member(message.chat.id, message.from_user.id)
-    if get.status in ["creator", "administrator"]:
+    if get.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
         if message.chat.id in stiklok:
             return await message.reply_text(f"يا {message.from_user.mention} الملصقات مقفله من قبل")
         stiklok.append(message.chat.id)
