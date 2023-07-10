e98828f6c88a026e94fe34e5532c54a86c594ba5
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-07-10 19:00:56 +0200

    Update نداء.py

--- AnonX/plugins/نداء.py
+++ AnonX/plugins/نداء.py
@@ -1,20 +1,16 @@
+
 import asyncio
-
-
 import random
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import filters, Client
 from AnonX import app
-from pyrogram.types import (InlineKeyboardButton,
-                            InlineKeyboardMarkup, Message)
 from strings.filters import command
-from pyrogram import Client
-from config import OWNER_ID
-from pyrogram import filters
 
-@app.on_message(command(["نداء","ن"]))
+@app.on_message(command(['نداء','ن']))
 def call_random_member(client:Client, message:Message):
     chat_id = message.chat.id
     members = [
-        member for member in client.get_chat_members(chat_id)
+        member for member in client.iter_chat_members(chat_id)
         if not member.user.is_bot
     ]
     random_member = random.choice(members)
@@ -26,6 +22,5 @@ def call_random_member(client:Client, message:Message):
         f"• يـا راس السطل تعال {random_member_mention}",
         f"• انت ليش قمر هكي 🌚♥ {random_member_mention}"
     ])
-    # client.send_message(chat_id, random_message, reply_to_message_id=message.message_id, parse_mode='markdown')
-
+    client.send_message(chat_id, random_message, reply_to_message_id= message.id, parse_mode='markdown')
 
