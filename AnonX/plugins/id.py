84db4d091de1749939306016811dcd39a9a44f58
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-07-11 02:46:00 +0200

    Update id.py

--- AnonX/plugins/id.py
+++ AnonX/plugins/id.py
@@ -72,7 +72,7 @@ async def iddd(client, message):
     usr = await client.get_chat(message.from_user.id)
     name = usr.first_name
     photo = await app.download_media(usr.photo.big_file_id)
-    await message.reply_photo(photo,       caption=f"""{xtxk}\n✧ ¦آســمــگڪ :{message.from_user.mention}\n✧ ¦يـوزرڪ :@{message.from_user.username}\n✧ ¦آيـديــڪ :`{message.from_user.id}`\n✧ ¦بـآيـو :{usr.bio}\n✧ ¦ࢪتبتگ: {rotba}""",
+    await message.reply_photo(photo,       caption=f"""**{xtxk}\n✧ ¦آســمــگڪ :{message.from_user.mention}\n✧ ¦يـوزرڪ :@{message.from_user.username}\n✧ ¦آيـديــڪ :`{message.from_user.id}`\n✧ ¦بـآيـو :{usr.bio}\n✧ ¦ࢪتبتگ: {rotba}**""",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
