6af444260a0e5cf382ccea5d4dfd18872a81ba20
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-07-12 17:45:20 +0200

    Update stop.py

--- AnonX/plugins/stop.py
+++ AnonX/plugins/stop.py
@@ -15,7 +15,8 @@ STOP_COMMAND = get_command("STOP_COMMAND")
 
 @app.on_message(
     command(STOP_COMMAND)
-    & ~filters.group
+    & filters.group
+    & ~filters.channel
     & ~BANNED_USERS
 )
 @AdminRightsCheck
@@ -24,7 +25,3 @@ async def stop_music(cli, message: Message, _, chat_id):
         return await message.reply_text(_["general_2"])
     await Anon.stop_stream(chat_id)
     await set_loop(chat_id, 0)
-    await message.reply_text(
-        _["admin_9"].format(message.from_user.first_name),
-        reply_markup=close_keyboard,
-    )
