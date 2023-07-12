243e804b8f40169da39e3e9e78d16d97fb9944bf
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-07-12 18:56:22 +0200

    Update admins.py

--- AnonX/utils/decorators/admins.py
+++ AnonX/utils/decorators/admins.py
@@ -1,137 +1,137 @@
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from pyrogram.enums import ChatType
-from pyrogram import Client
-from config import adminlist
-from strings import get_string
-from AnonX import app
-from pyrogram import enums
-from AnonX.misc import SUDOERS
-from AnonX.utils.database import (get_authuser_names, get_cmode,
-                                       get_lang, is_active_chat,
-                                       is_commanddelete_on,
-                                       is_maintenance,
-                                       is_nonadmin_chat)
+# from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+# from pyrogram.enums import ChatType
+# from pyrogram import Client
+# from config import adminlist
+# from strings import get_string
+# from AnonX import app
+# from pyrogram import enums
+# from AnonX.misc import SUDOERS
+# from AnonX.utils.database import (get_authuser_names, get_cmode,
+#                                        get_lang, is_active_chat,
+#                                        is_commanddelete_on,
+#                                        is_maintenance,
+#                                        is_nonadmin_chat)
 
-from ..formatters import int_to_alpha
+# from ..formatters import int_to_alpha
 
 
-def AdminRightsCheck(mystic):
-    async def wrapper(client:Client, message:Message):
+# def AdminRightsCheck(mystic):
+#     async def wrapper(client:Client, message:Message):
         
-        if await is_commanddelete_on(message.chat.id):
-            try:
-                await message.delete()
-            except:
-                pass
-        try:
-            language = await get_lang(message.chat.id)
-            _ = get_string(language)
-        except:
-            _ = get_string("en")
+#         if await is_commanddelete_on(message.chat.id):
+#             try:
+#                 await message.delete()
+#             except:
+#                 pass
+#         try:
+#             language = await get_lang(message.chat.id)
+#             _ = get_string(language)
+#         except:
+#             _ = get_string("en")
   
-        if message.command[0][0] == "c":
-            chat_id = await get_cmode(message.chat.id)
-            if chat_id is None:
-                return await message.reply_text(_["setting_12"])
-            try:
-                await app.get_chat(chat_id)
-            except:
-                return await message.reply_text(_["cplay_4"])
-        else:
-            chat_id = message.chat.id
-        if not await is_active_chat(chat_id):
-            return await message.reply_text(_["general_6"])
-        is_non_admin = await is_nonadmin_chat(message.chat.id)
-        if not is_non_admin:
-                admins = adminlist.get(message.chat.id)
-                if not admins:
-                  chat_id = message.chat.id
-                  adminlist[chat_id] = []
-                  async for user in app.get_chat_members(chat_id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
-                       if user.privileges.can_manage_video_chats == True:
-                          adminlist[chat_id].append(user.user.id)
-                else:
-                    if message.from_user.id not in admins:
-                        return await message.reply_text(_["admin_19"])
-        return await mystic(client, message, _, chat_id)
+#         if message.command[0][0] == "c":
+#             chat_id = await get_cmode(message.chat.id)
+#             if chat_id is None:
+#                 return await message.reply_text(_["setting_12"])
+#             try:
+#                 await app.get_chat(chat_id)
+#             except:
+#                 return await message.reply_text(_["cplay_4"])
+#         else:
+#             chat_id = message.chat.id
+#         if not await is_active_chat(chat_id):
+#             return await message.reply_text(_["general_6"])
+#         is_non_admin = await is_nonadmin_chat(message.chat.id)
+#         if not is_non_admin:
+#                 admins = adminlist.get(message.chat.id)
+#                 if not admins:
+#                   chat_id = message.chat.id
+#                   adminlist[chat_id] = []
+#                   async for user in app.get_chat_members(chat_id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
+#                        if user.privileges.can_manage_video_chats == True:
+#                           adminlist[chat_id].append(user.user.id)
+#                 else:
+#                     if message.from_user.id not in admins:
+#                         return await message.reply_text(_["admin_19"])
+#         return await mystic(client, message, _, chat_id)
 
-    return wrapper
+#     return wrapper
 
 
-def AdminActual(mystic):
-    async def wrapper(client:Client, message:Message):
-        if await is_maintenance() is False:
+# def AdminActual(mystic):
+#     async def wrapper(client:Client, message:Message):
+#         if await is_maintenance() is False:
       
-                return await message.reply_text(
-                    "» ʙᴏᴛ ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ ғᴏʀ sᴏᴍᴇ ᴛɪᴍᴇ, ᴩʟᴇᴀsᴇ ᴠɪsɪᴛ sᴜᴩᴩᴏʀᴛ ᴄʜᴀᴛ ᴛᴏ ᴋɴᴏᴡ ᴛʜᴇ ʀᴇᴀsᴏɴ."
-                )
-        if await is_commanddelete_on(message.chat.id):
-            try:
-                await message.delete()
-            except:
-                pass
-        try:
-            language = await get_lang(message.chat.id)
-            _ = get_string(language)
-        except:
-            _ = get_string("en")
+#                 return await message.reply_text(
+#                     "» ʙᴏᴛ ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ ғᴏʀ sᴏᴍᴇ ᴛɪᴍᴇ, ᴩʟᴇᴀsᴇ ᴠɪsɪᴛ sᴜᴩᴩᴏʀᴛ ᴄʜᴀᴛ ᴛᴏ ᴋɴᴏᴡ ᴛʜᴇ ʀᴇᴀsᴏɴ."
+#                 )
+#         if await is_commanddelete_on(message.chat.id):
+#             try:
+#                 await message.delete()
+#             except:
+#                 pass
+#         try:
+#             language = await get_lang(message.chat.id)
+#             _ = get_string(language)
+#         except:
+#             _ = get_string("en")
   
  
-            try:
-                member = await app.get_chat_member(
-                    message.chat.id, message.from_user.id
-                )
-            except:
-                return
-            if not member.privileges.can_manage_video_chats:
-                return await message.reply(_["general_5"])
-        return await mystic(client, message, _)
+#             try:
+#                 member = await app.get_chat_member(
+#                     message.chat.id, message.from_user.id
+#                 )
+#             except:
+#                 return
+#             if not member.privileges.can_manage_video_chats:
+#                 return await message.reply(_["general_5"])
+#         return await mystic(client, message, _)
 
-    return wrapper
+#     return wrapper
 
 
-def ActualAdminCB(mystic):
-    async def wrapper(client:Client, CallbackQuery):
-        if await is_maintenance() is False:
-                return await CallbackQuery.answer(
-                    "» ʙᴏᴛ ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ ғᴏʀ sᴏᴍᴇ ᴛɪᴍᴇ, ᴩʟᴇᴀsᴇ ᴠɪsɪᴛ sᴜᴩᴩᴏʀᴛ ᴄʜᴀᴛ ᴛᴏ ᴋɴᴏᴡ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
-                    show_alert=True,
-                )
-        try:
-            language = await get_lang(CallbackQuery.message.chat.id)
-            _ = get_string(language)
-        except:
-            _ = get_string("en")
-        if CallbackQuery.message.chat.type == ChatType.PRIVATE:
-            return await mystic(client, CallbackQuery, _)
-        is_non_admin = await is_nonadmin_chat(
-            CallbackQuery.message.chat.id
-        )
-        if not is_non_admin:
-            try:
-                a = await app.get_chat_member(
-                    CallbackQuery.message.chat.id,
-                    CallbackQuery.from_user.id,
-                )
-            except:
-                return await CallbackQuery.answer(
-                    _["general_5"], show_alert=True
-                )
-            if not a.privileges.can_manage_video_chats:
-                    token = await int_to_alpha(
-                        CallbackQuery.from_user.id
-                    )
-                    _check = await get_authuser_names(
-                        CallbackQuery.from_user.id
-                    )
-                    if token not in _check:
-                        try:
-                            return await CallbackQuery.answer(
-                                _["general_5"],
-                                show_alert=True,
-                            )
-                        except:
-                            return
-        return await mystic(client, CallbackQuery, _)
+# def ActualAdminCB(mystic):
+#     async def wrapper(client:Client, CallbackQuery):
+#         if await is_maintenance() is False:
+#                 return await CallbackQuery.answer(
+#                     "» ʙᴏᴛ ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ ғᴏʀ sᴏᴍᴇ ᴛɪᴍᴇ, ᴩʟᴇᴀsᴇ ᴠɪsɪᴛ sᴜᴩᴩᴏʀᴛ ᴄʜᴀᴛ ᴛᴏ ᴋɴᴏᴡ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
+#                     show_alert=True,
+#                 )
+#         try:
+#             language = await get_lang(CallbackQuery.message.chat.id)
+#             _ = get_string(language)
+#         except:
+#             _ = get_string("en")
+#         if CallbackQuery.message.chat.type == ChatType.PRIVATE:
+#             return await mystic(client, CallbackQuery, _)
+#         is_non_admin = await is_nonadmin_chat(
+#             CallbackQuery.message.chat.id
+#         )
+#         if not is_non_admin:
+#             try:
+#                 a = await app.get_chat_member(
+#                     CallbackQuery.message.chat.id,
+#                     CallbackQuery.from_user.id,
+#                 )
+#             except:
+#                 return await CallbackQuery.answer(
+#                     _["general_5"], show_alert=True
+#                 )
+#             if not a.privileges.can_manage_video_chats:
+#                     token = await int_to_alpha(
+#                         CallbackQuery.from_user.id
+#                     )
+#                     _check = await get_authuser_names(
+#                         CallbackQuery.from_user.id
+#                     )
+#                     if token not in _check:
+#                         try:
+#                             return await CallbackQuery.answer(
+#                                 _["general_5"],
+#                                 show_alert=True,
+#                             )
+#                         except:
+#                             return
+#         return await mystic(client, CallbackQuery, _)
 
-    return wrapper
+#     return wrapper
