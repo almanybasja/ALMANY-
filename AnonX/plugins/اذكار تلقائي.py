48069d382d46af1406313e8468e7294002c38ab7
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-07-12 03:49:54 +0200

    Update اذكار تلقائي.py

--- AnonX/plugins/اذكار تلقائي.py
+++ AnonX/plugins/اذكار تلقائي.py
@@ -1,34 +1,35 @@
 import time
 from pyrogram import Client, filters
-
-app = Client("my_account")
-
-# المعرف الخاص بالمجموعة المراد إرسال الرسالة فيها
-group_id = -100123456789
+from pyrogram.types import Message
 
 
-message_text = "تم إرسال رسالة تلقائية من خلال الكود الخاص بي!"
 
-# المتغير الذي يمكننا استخدامه لتعطيل أو تفعيل الكود
+
+
+message_text = [
+    
+]
+
+
 enable_code = True
 
-@app.on_message(filters.command("تفعيل", prefixes="/") & ~filters.private)
+@app.on_message(command("تفعيل") & ~filters.group)
 def enable_code_fn(client, message):
     global enable_code
     enable_code = True
     message.reply_text("تم تفعيل الكود بنجاح!")
 
-@app.on_message(filters.command("تعطيل", prefixes="/") & ~filters.private)
-def disable_code_fn(client, message):
+@app.on_message(command("تعطيل") & ~filters.group)
+def disable_code_fn(client: Client, message:Message):
     global enable_code
     enable_code = False
     message.reply_text("تم تعطيل الكود بنجاح!")
 
-@app.on_message(filters.private)
-def send_message(client, message):
+@app.on_message(filters.group)
+def send_message(client: Client, message:Message):
     if enable_code:
-        # إرسال الرسالة في المجموعة
-        app.send_message(group_id, message_text)
+       
+        app.send_message(message.chat.id, message_text)
 
-        # انتظار 5 دقائق قبل إرسال الرسالة التالية
+        
         time.sleep(5 * 60)
