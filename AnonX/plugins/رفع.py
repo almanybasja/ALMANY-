52ca8e5f58b9ab06da7d0de32c36d3d0a3fd4e81
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-07-15 11:12:21 +0200

    Update رفع.py

--- AnonX/plugins/رفع.py
+++ AnonX/plugins/رفع.py
@@ -16,9 +16,9 @@ lokrf = []
 )
 async def lllock(client, message):
     dev = (OWNER_ID)
-    
+    haya = (6275847466)
     get = await client.get_chat_member(message.chat.id, message.from_user.id)
-    if message.from_user.id == '6275847466':
+    if message.from_user.id in haya:
          rotba= "مّمٌَـبـ ـࢪمـج السوࢪس" 
     elif  message.from_user.id in dev:
          rotba = "مطور اساسي"
@@ -40,8 +40,9 @@ async def lllock(client, message):
 )
 async def idljjopen(client, message):
     dev = (OWNER_ID)
+    haya = (6275847466)
     get = await client.get_chat_member(message.chat.id, message.from_user.id)
-    if message.from_user.id == '6275847466':
+    if message.from_user.id in haya:
          rotba= "مّمٌَـبـ ـࢪمـج السوࢪس" 
     elif  message.from_user.id in dev:
          rotba = "مطور اساسي"
@@ -67,7 +68,8 @@ klb = []
 
 @app.on_message(command("رفع كلب"))
 async def rf3nmla(client, message:Message):
-  if message.from_user.id == "6275847466":
+  haya = (6275847466)
+  if message.from_user.id in haya:
     return await message.reply_text("اححممم هذا مبرمجي كيف تبيني نرفعه انت الكلب 💔🥺")   
   if not message.reply_to_message.from_user.mention in klb:
     klb.append(message.reply_to_message.from_user.mention)
@@ -93,7 +95,8 @@ zoj = []
 
 @app.on_message(command("رفع زوجي"))
 async def rf3nmla(client, message:Message):
-  if message.from_user.id == "6275847466":
+  haya = (6275847466)
+  if message.from_user.id in haya:
     return await message.reply_text("مبرمجي يستاهل بنت خير منك يلي راسك مربع 😃😃")   
   if not message.reply_to_message.from_user.mention in zoj:
     zoj.append(message.reply_to_message.from_user.mention)
@@ -119,7 +122,8 @@ hth =[]
 
 @app.on_message(command("رفع حثاله"))
 async def rf3nmla(client, message:Message):
-  if message.from_user.id == "6275847466":
+  haya = (6275847466)
+  if message.from_user.id in haya:
      await message.reply_text("انت الحثاله هذا افضل مبرمج الوسكي عمك 😉😉😌")   
   if not message.reply_to_message.from_user.mention in hth:
     hth.append(message.reply_to_message.from_user.mention)
@@ -146,7 +150,8 @@ zog =[]
 
 @app.on_message(command("رفع زوجتي"))
 async def rf3nmla(client, message:Message):
-  if message.from_user.id == "6275847466":
+  haya = (6275847466)
+  if message.from_user.id in haya:
      await message.reply_text("شن تحساب مبرمجي؟؟")   
   if not message.reply_to_message.from_user.mention in zog:
     zog.append(message.reply_to_message.from_user.mention)
