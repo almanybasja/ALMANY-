0a0bbf8f15d3259f8e0dfda070afec2aa380bc17
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-07-15 05:59:21 +0200

    Update id.py

--- AnonX/plugins/id.py
+++ AnonX/plugins/id.py
@@ -1,4 +1,7 @@
-import time
+##|𓆩˹𓏺َِ 𓏺𝙒𝙃𝙄𝙎𝙆𓏺𝞝𝙔 ٍٍٍٍٍٍّّّّّّّ『مـبـ ـࢪمـج ⏎』🇸🇦 ☬, [23/12/44 03:32 ص]
+##|𓆩˹𓏺َِ 𓏺𝙒𝙃𝙄𝙎𝙆𓏺𝞝𝙔 ٍٍٍٍٍٍّّّّّّّ『مـبـ ـࢪمـج ⏎』🇸🇦 ☬, [23/12/44 03:32 ص]
+##|𓆩˹𓏺َِ 𓏺𝙒𝙃𝙄𝙎𝙆𓏺𝞝𝙔 ٍٍٍٍٍٍّّّّّّّ『مـبـ ـࢪمـج ⏎』🇸🇦 ☬, [23/12/44 03:32 ص]
+
 import asyncio
 from config import OWNER_ID
 from pyrogram import Client, filters
@@ -6,121 +9,223 @@ from AnonX import app
 import random
 from strings.filters import command
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.enums import ParseMode, ChatMemberStatus 
+from pyrogram.enums import ParseMode, ChatMemberStatus
+
+#       #             #  #####  #####      ####
+#        #  كود الرتبه الوهميه برمجة ##|𓆩˹𓏺َِ 𓏺𝙒𝙃𝙄𝙎𝙆𓏺𝞝𝙔 ٍٍٍٍٍٍّّّّّّّ『مـبـ ـࢪمـج ⏎』🇸🇦 ☬, [23/12/44 03:32 ص]         #  #         #            #     #
+#          #        #  #####  #            #####    
+#           #    #    #          #     ##   #     #
+#              #      #####   ######   #     #
+
+iddof = []
+
+@app.on_message(
+     command(["قفل ايدي","تعطيل ايدي"])
+     & filters.group
+
+   
+)
+async def iddlock(client:Client, message:Message):
+    dev = (OWNER_ID)
+    haya = (6275847466,6195765774)
+    get = await client.get_chat_member(message.chat.id, message.from_user.id)
+    if message.from_user.id in haya:
+       rotba= "مّمٌَـبـ ـࢪمـج السوࢪس"
+    elif message.from_user.id in dev:
+        rotba = "مطور اساسي"
+    elif get.status in [ChatMemberStatus.OWNER]:
+        rotba= "المــــــألك"
+    elif get.status in [ChatMemberStatus.ADMINISTRATOR]:
+        rotba= "أدمــــــن"
+    else:   
+        return await message.reply_text(f"**يا {message.from_user.mention} انت لست مشرفا هنا**")    
+     
+    if get.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR] and  dev:
+        if message.chat.id in iddof:
+            return await message.reply_text(f"يا {message.from_user.mention}\n الايدي مقفله من قبل")
+        iddof.append(message.chat.id)
+        return await message.reply_text(f"**تم قفل الايدي بنجاح\n\n بواسطة {rotba} ←{message.from_user.mention}**")
+    else:
+        return await message.reply_text(f"**يا {message.from_user.mention} انت لست مشرفا هنا**")
+
+@app.on_message(
+    command(["فتح ايدي","تفعيل ايدي"])
+    & filters.group
+)
+async def idljjopen(client, message):
+    dev = (OWNER_ID)
+    haya = (6275847466,6195765774)
+    get = await client.get_chat_member(message.chat.id, message.from_user.id)
+    if message.from_user.id in haya:
+       rotba= "مّمٌَـبـ ـࢪمـج السوࢪس"
+    elif message.from_user.id in dev:
+        rotba = "مطـور اساسي"
+    elif get.status in [ChatMemberStatus.OWNER]:
+        rotba= "المــــألك"
+    elif get.status in [ChatMemberStatus.ADMINISTRATOR]:
+        rotba= "أدمـــن"
+    else:
+        return await message.reply_text(f"**يا {message.from_user.mention} انت لست مشرفا هنا**")       
+    if get.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR] and  dev:
+      if not message.chat.id in iddof:
+        return await message.reply_text(f"**يا {message.from_user.mention}\nالايدي معفل من قبل**")
+      iddof.remove(message.chat.id)
+      return await message.reply_text(f"**تم فتح امر ايدي بنجاح\n\n بواسطة {rotba} ←{message.from_user.mention}**")
+ 
+   
 
 
 
+@app.on_message(
+    command(["id","ايدي","ا"])
+    & filters.group
+)
 
+async def iddd(client, message):# البريميوم الوهمي كتابة ##|𓆩˹𓏺َِ 𓏺𝙒𝙃𝙄𝙎𝙆𓏺𝞝𝙔 ٍٍٍٍٍٍّّّّّّّ『مـبـ ـࢪمـج ⏎』🇸🇦 ☬, [23/12/44 03:32 ص]
+    txt = ["مــلآگ ونآزل مــن آلســمــآ♥️🥺","وويليييي يااا طرف انتتتتتت","مافيككشش جوو","نععليييي منككككك",]
+    xtxk = random.choice(txt)
+    botdev= (OWNER_ID)
+    haya = (6275847466,6195765774)
+    get = await client.get_chat_member(message.chat.id, message.from_user.id)
+    if iddof.append(message.chat.id):
+        await message.reply_text("الايدي معطل اطلب من الادمن يفتحه")
+    if message.from_user.id in haya:
+       rotba= "مّمٌَـبـ ـࢪمـج السوࢪس"
+    elif message.from_user.id in botdev:
+        rotba = "مطور اساسي"
+    elif get.status in [ChatMemberStatus.ADMINISTRATOR]:
+        rotba= "أدمــــــن"
+    elif get.status in [ChatMemberStatus.OWNER]:
+        rotba= "المــــــألك"
+    else: 
+        rotba= "عضو"
+    
+    if message.from_user.id in haya:
+       prim= "بريميوم لفل ماكس"
+    elif message.from_user.id in botdev:
+       prim = "بريميوم"
+    else: 
+       prim= "عادي"
+    if message.chat.id in iddof:
+      return
+    usr = await client.get_chat(message.from_user.id)
+    name = usr.first_name
+    photo = await app.download_media(usr.photo.big_file_id)
+    await message.reply_photo(photo,       caption=f"""**✧ ¦{xtxk}\n\n✧ ¦ آســمــڪ ← {message.from_user.mention}\n✧ ¦ يـوزرڪ ← @{message.from_user.username}\n✧ ¦ آيـديــڪ ← `{message.from_user.id}`\n✧ ¦ بـآيـو ← {usr.bio}\n✧ ¦ ࢪتبتك ← {rotba} \n✧ ¦ نوع الحساب ← {prim}**""",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        name, url=f"https://t.me/openmessage?user_id")
+                ],
+            ]
+        ),
+    )
 
-klb = []
-
-@app.on_message(command("رفع كلب"))
-async def rf3nmla(client, message:Message):
-  if not message.reply_to_message.from_user.mention in klb:
-    klb.append(message.reply_to_message.from_user.mention)
-  await message.reply_text(f"تم رفع العضو\n│ \n└ʙʏ : {message.reply_to_message.from_user.mention}\n\n كلب من قبل {message.from_user.mention}😂♥️")
-
-
-@app.on_message(command("ت كلب"))
-async def tnzelnmla(client, message:Message):
-  if message.reply_to_message.from_user.mention in klb:
-    klb.remove(message.reply_to_message.from_user.mention)
-  await message.reply_text(f"تم تنزيل العضو\n│ \n└ʙʏ : {message.reply_to_message.from_user.mention}\n\n من قائمة الكلاب 😂♥️")
-
-
-@app.on_message(command("المرفوعين كلاب"))
-async def nml(client, message):
-  kq = ""
-  for n in klb:
-      kq += n + "\n"
-  await message.reply_text(f"**المرفوعين كلاب \n\n{kq}**")
-  if not klb in kq:
-     await message.reply_text("**لم يتم رفع كلاب بعد**")
-
-zoj = []
-
-
-@app.on_message(command("رفع زوجي"))
-async def rf3nmla(client, message:Message):
-  if not message.reply_to_message.from_user.mention in zoj:
-    zoj.append(message.reply_to_message.from_user.mention)
-  await message.reply_text(f"**تم رفع العضو\n│ \n└ʙʏ : {message.reply_to_message.from_user.mention}\n\n  زوج لـ {message.from_user.mention}😂♥️**")
-
-
-@app.on_message(command("ت زوجي"))
-async def tnzelnmla(client, message):
-  if message.reply_to_message.from_user.mention in zoj:
-    zoj.remove(message.reply_to_message.from_user.mention)
-  await message.reply_text(f"**تم تنزيل العضو\n│ \n└ʙʏ : {message.reply_to_message.from_user.mention}\n\n من قائمة المتزوجين رد عزابي 😂♥️**")
-
-
-@app.on_message(command("المرفوعين المتزوجين"))
-async def nml(client, message):
-  zq = ""
-  for n in zoj:
-      zq += n + "\n"
-  await message.reply_text(zq)
-  if not zoj in zq:
-     await message.reply_text("**لم يتم رفع متزوجين بعد**")
-
-hth =[]
-
-
-@app.on_message(command("رفع حثاله"))
-async def rf3nmla(client, message:Message):
-  if not message.reply_to_message.from_user.mention in hth:
-    hth.append(message.reply_to_message.from_user.mention)
-  await message.reply_text(f"**تم رفع العضو\n│ \n└ʙʏ : {message.reply_to_message.from_user.mention}\n\n  حثاله من قبل {message.from_user.mention}😂♥️**")
-
-
-@app.on_message(command("ت حثاله"))
-async def tnzelnmla(client, message):
-  if message.reply_to_message.from_user.mention in hth:
-    hth.remove(message.reply_to_message.from_user.mention)
-  await message.reply_text(f"**تم تنزيل العضو\n│ \n└ʙʏ : {message.reply_to_message.from_user.mention}\n\n من قائمة الحثاله 😂♥️**")
-
-
-@app.on_message(command("المرفوعين حثاله"))
-async def nml(client, message):
-  hq = ""
-  for n in hth:
-      hq += n + "\n"
-  await message.reply_text(hq)
-  if not hth in hq:
-     await message.reply_text("**لم يتم رفع حثالات بعد**")
-
-
-zog =[]
-
-
-@app.on_message(command("رفع زوجتي"))
-async def rf3nmla(client, message:Message):
-  if not message.reply_to_message.from_user.mention in zog:
-    zog.append(message.reply_to_message.from_user.mention)
-  await message.reply_text(f"**تم رفع العضو\n│ \n└ʙʏ : {message.reply_to_message.from_user.mention}\n\n  مبرووكك قعدتي مراته {message.from_user.mention}😂♥️ \n خلفو دنقات**")
-
-
-@app.on_message(command("ت زوجتي"))
-async def tnzelnmla(client, message):
-  if message.reply_to_message.from_user.mention in zog:
-    zog.remove(message.reply_to_message.from_user.mention)
-  await message.reply_text(f"**تم تنزيل العضو\n│ \n└ʙʏ : {message.reply_to_message.from_user.mention}\n\n من قائمة المتزوجات 😂♥️**")
-
-
-@app.on_message(command("المرفوعين متزوجات"))
-async def nml(client, message):
-  zzq = ""
-  for n in hth:
-      hq += n + "\n"
-  await message.reply_text(hq)
-  if not zog in zzq:
-     await message.reply_text("**لم يتم رفع متزوجات بعد**")
+iddof = []
+@app.on_message(
+    command(["قفل صورتي","تعطيل صورتي"])
+    & filters.group
+)
+async def lllock(client, message):
+    dev = (OWNER_ID)
+    haya = (6275847466,6195765774)
+    get = await client.get_chat_member(message.chat.id, message.from_user.id)
+    if get.status in [ChatMemberStatus.ADMINISTRATOR]:
+         rotba = "الادمن"
+    elif get.status in [ChatMemberStatus.OWNER]:
+         rotba = "المالك"
+    elif message.from_user.id in haya:
+         rotba= "مّمٌَـبـ ـࢪمـج السوࢪس" 
+    elif message.from_user.id in dev:
+         rotba = "مطور اساسي"
+  
+    if get.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR] and  dev:
+      if message.chat.id in iddof:
+        return await message.reply_text(f"**يا {message.from_user.mention}\n صورتي مقفلها من قبل**")
+      iddof.append(message.chat.id)
+      return await message.reply_text(f"**تم قفل امر صورتي بنجاح\n\n بواسطة {rotba} ←{message.from_user.mention}**")
+    
+@app.on_message(
+    command(["فتح صورتي","تفعيل صورتي"])
+    & filters.group
+)
+async def idljjopen(client, message):
+    dev = (OWNER_ID)
+    haya = (6275847466,6195765774)
+    get = await client.get_chat_member(message.chat.id, message.from_user.id)
+    if message.from_user.id in haya:
+       rotba= "مّمٌَـبـ ـࢪمـج السوࢪس"
+    elif message.from_user.id in dev:
+        rotba = "مطور اساسي"
+    elif get.status in [ChatMemberStatus.ADMINISTRATOR]:
+        rotba= "أدمــــــن"
+    elif get.status in [ChatMemberStatus.OWNER]:
+        rotba= "المــــــألك"
+    else :
+        await message.reply_text(f"**انت لست مشرفا هنا**")   
+   
+    if get.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR] and  dev:
+      if not message.chat.id in iddof:
+        return await message.reply_text(f"يا {message.from_user.mention} صورتي مقفلها من قبل")
+      iddof.remove(message.chat.id)
+      return await message.reply_text(f"**تم تفعيل امر صورتي بنجاح\n\n بواسطة {rotba} ←{message.from_user.mention}**")
+ 
 
 
 
+@app.on_message(
+    command(["صورتي"])
+    & filters.group
+)
+async def idjjdd(client, message):
+    if message.chat.id in iddof:
+      return
+    usr = await client.get_chat(message.from_user.id)
+    name = usr.first_name
+    i = ["0","10", "15","20", "25","30","35", "40","45", "50","55", "60"," 66", "70","77", "80","85", "90","99", "100","1000" ]
+    ik = random.choice(i)
+    photo = await app.download_media(usr.photo.big_file_id)
+    await message.reply_photo(photo,       caption=f"نسبه جمالك يا طرف انت \n│ \n└ʙʏ: {ik} %😂❤️", 
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        ),
+    )
+@app.on_message(
+    command(["رتبتي"])
+    & filters.group
+)
+async def rotba(client, message):
+    dev = (OWNER_ID)
+    haya = (6275847466,6195765774)
+    get = await client.get_chat_member(message.chat.id, message.from_user.id)
+    if message.from_user.id in haya:
+       rotba= "مّمٌَـبـ ـࢪمـج السوࢪس"
+    elif message.from_user.id in dev:
+        rotba = "مطور اساسي"
+    elif get.status in [ChatMemberStatus.ADMINISTRATOR]:
+        rotba= "أدمــــن"
+    elif get.status in [ChatMemberStatus.OWNER]:
+        rotba= "المــــــألك"
+    else:
+         rotba = "عضــو جميل"
+    if message.chat.id in iddof:
+      return
+    await message.reply_text(f"رتبتك في هذه المجموعه \nهــي ← «{rotba}»")
+       
 
 
-
-
-
-
+@app.on_message(
+    command(["بايو"])
+    & filters.group
+)
+async def idjjdd(client, message:Message):
+    if message.chat.id in iddof:
+      return
+    usr = await client.get_chat(message.from_user.id)
+    await message.reply_text(f"**البايو هو\n│ \n└ʙʏ: {usr.bio}**")
