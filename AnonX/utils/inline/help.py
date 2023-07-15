3bdaacaf247b02d1a8059a05b3fc7638d7ea8587
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-07-15 05:24:59 +0200

    Update help.py

--- AnonX/utils/inline/help.py
+++ AnonX/utils/inline/help.py
@@ -23,60 +23,23 @@ def help_pannel(_, START: Union[bool, int] = None):
         [
             [
                 InlineKeyboardButton(
-                    text="[𖢿اوامر الادمن𖢿]",
+                    text="[اوامر القفل]",
                     callback_data="help_callback hb1",
                 ),
                 InlineKeyboardButton(
-                    text="[𖢿المصادقه𖢿]",
+                    text="[اوامر الفتح]",
                     callback_data="help_callback hb2",
                 ),
-                InlineKeyboardButton(
-                    text="[𖢿القائمه السوداء𖢿]",
-                    callback_data="help_callback hb3",
-                ),
             ],
             [
-                InlineKeyboardButton(
-                    text="[𖢿الاذاعه𖢿]",
-                    callback_data="help_callback hb4",
-                ),
-                InlineKeyboardButton(
-                    text="[𖢿المحظورين𖢿]",
-                    callback_data="help_callback hb12",
-                ),
-                InlineKeyboardButton(
-                    text="[𖢿كلمات𖢿]",
-                    callback_data="help_callback hb5",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text="[𖢿بينج𖢿]",
-                    callback_data="help_callback hb7",
-                ),
+             
                 InlineKeyboardButton(
                     text="[𖢿تشغيل𖢿]",
                     callback_data="help_callback hb8",
                 ),
-                InlineKeyboardButton(
-                    text="[𖢿قائمة التشغيل𖢿]",
-                    callback_data="help_callback hb6",
-                ),
+             
             ],
-            [
-                InlineKeyboardButton(
-                    text="[𖢿المكالمات الصوتيه𖢿]",
-                    callback_data="help_callback hb10",
-                ),
-                InlineKeyboardButton(
-                    text="[𖢿ستارت𖢿]",
-                    callback_data="help_callback hb11",
-                ),
-                InlineKeyboardButton(
-                    text="[𖢿اوامر المطورين𖢿]",
-                    callback_data="help_callback hb9",
-                ),
-            ],
+           
             mark,
         ]
     )
