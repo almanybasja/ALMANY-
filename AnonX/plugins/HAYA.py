e19e2e1d687667b3f4b9f7467c46083ef02a3eab
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-07-18 05:16:55 +0200

    Update HAYA.py

--- AnonX/plugins/HAYA.py
+++ AnonX/plugins/HAYA.py
@@ -202,35 +202,4 @@ async def huhh(client: Client, message: Message):
 
     )
 
-@app.on_message(command(["غنيلي", "غني", "غ", "حياه غنيلي"]))
-async def ihd(client: Client, message: Message):
-    rl = random.getrandbits(3,267)
-    url = f"https://t.me/bsmaatt/{rl}"
-    await client.send_voice(message.chat.id,url,caption="`🔥 ¦ تـم اختيـار الاغـنـية لـك`",parse_mode="html",
-    reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
-                ],
-            ]
-        )
-    )
-    
-@app.on_message(command(["صورة","صور"]))
-async def ihd(client: Client, message: Message):
-    rs = random.getrandbits(39,148)
-    url = f"https://t.me/GTTUTY/{rs}"
-    await client.send_photo(message.chat.id,url,caption="`💕 ¦ تـم اختيـار الصوره لـك`",parse_mode="html",
-    reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
-                ],
-            ]
-        )
-    )
-
-
     
