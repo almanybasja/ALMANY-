76965983b2fe1b2fdaf81c522cc72dca6854c0c8
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-07-19 09:24:59 +0200

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,6 @@ USER_OWNER = getenv("USER_OWNER","bp_bp")
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/ma7amad2000/HAYA01")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
-BOT_ID = int(getenv("bot_id"))
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/HL_BG")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/HL_BG")
 
