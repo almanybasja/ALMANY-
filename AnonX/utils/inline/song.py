3afa63f374718e5e2eba2a106b10cfb87570d941
almanybasja <149967867+almanybasja@users.noreply.github.com>
2023-12-06 15:51:09 +0200

    Update song.py

--- AnonX/utils/inline/song.py
+++ AnonX/utils/inline/song.py
@@ -15,7 +15,7 @@ def song_markup(_, vidid):
         ],
         [
             InlineKeyboardButton(
-                text="[𖢿التحديثات𖢿]", url=f"https://t.me/HL_BG",
+                text="[𖢿التحديثات𖢿]", url=f"https://t.me/AL515AT",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
